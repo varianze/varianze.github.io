5f01dabe306035269c180dca7d0bba798efe5ed7
Richard Ng <richardng@Richards-MacBook-Pro.local>
2020-03-21 00:50:52 +0800

    Updates

---
+++ precache-manifest.e3393d191a4e78bb3f24e2df85ca5f28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0ef9fb7ecd527aabf28a548b9f1bb60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7cbd8693fa66d06687b5",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "23af6a9ea2a3355793dc",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7cbd8693fa66d06687b5",
+    "url": "/static/js/2.654dd158.chunk.js"
+  },
+  {
+    "revision": "e64558e2f95ad6e9b52b3cecd9aea933",
+    "url": "/static/js/2.654dd158.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23af6a9ea2a3355793dc",
+    "url": "/static/js/main.5648078a.chunk.js"
+  },
+  {
+    "revision": "151406ead482bd85bd78",
+    "url": "/static/js/runtime-main.83b9d697.js"
+  }
+]);
